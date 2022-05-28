d341bf6a760cfe196d9b9ececf1330896971f7f0
Dany Corleone <corleonedany@live.fr>
2022-05-28 21:56:56 +0200

    docker now releases a release version, sentry test removed from urls

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -4,15 +4,9 @@ from django.urls import include, path
 from . import views
 
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
-    return division_by_zero
-
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path("lettings/", include("lettings.urls")),
     path("profiles/", include("profiles.urls")),
-    path('sentry-debug/', trigger_error),
 ]
