983cdd7ecfb30f95db8d554f660b86ac54e23fcc
Dany Corleone <corleonedany@live.fr>
2022-05-29 14:59:47 +0200

    dsn default set to secretkey

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 sentry_sdk.init(
-    dsn=config('SENTRY_DSN', default="https:secretdsn.com"),
+    dsn=config('SENTRY_DSN', default="secretkey"),
     integrations=[DjangoIntegration()],
 
     # Set traces_sample_rate to 1.0 to capture 100%
