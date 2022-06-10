1c3baa3e5cbe2a0bd6a7d0168eaa4503429eae0e
CorleoneDany <corleonedany@live.fr>
2022-06-10 19:12:06 +0200

    fixed url error, updated wsgi file to include whitenoise

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
-import settings
+from django.conf import settings
 
 from . import views
 
@@ -12,3 +12,7 @@ urlpatterns = [
     path("lettings/", include("lettings.urls")),
     path("profiles/", include("profiles.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
