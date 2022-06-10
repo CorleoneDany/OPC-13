5c305e96a9014dcaaf56cac05c0ccb1c8917bdc4
CorleoneDany <corleonedany@live.fr>
2022-06-10 19:13:05 +0200

    fixed url file

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -12,7 +12,3 @@ urlpatterns = [
     path("lettings/", include("lettings.urls")),
     path("profiles/", include("profiles.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
